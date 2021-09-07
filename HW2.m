94ba72adefb9f9e622f9b92b516543fb27ecac2e
John Muirhea <jmuir5125@gmail.com>
2021-09-07 10:31:59 -0400

    first commit

---
+++ HW2.m
@@ -0,0 +1,18 @@
+classdef HW2
+    %This provides the code for HW2
+    
+    properties
+        
+    end
+    
+    methods
+        function obj = untitled(self,inputArg2)
+            
+        end
+        
+        function outputArg = method1(self,inputArg)
+            
+        end
+    end
+end
+
