bb7e6359600667de9c5ecac97ebf7bba6a0417fa
John Muirhea <jmuir5125@gmail.com>
2021-09-07 10:48:36 -0400

    added in the first function

--- HW2.m
+++ HW2.m
@@ -6,13 +6,43 @@ classdef HW2
     end
     
     methods
-        function obj = untitled(self,inputArg2)
-            
+        
+        function T = trotz(~, theta)
+            T = zeros(4,4);
+            for i=1:4
+                T(i,i) = 1;                
+            end
+            T(1,1) = cos(theta);
+            T(1,2) = sin(theta);
+            T(2,1) = -sin(theta);
+            T(2,2) = cos(theta);
         end
         
-        function outputArg = method1(self,inputArg)
-            
+        function T = troty(~,theta)
+            T = zeros(4,4);
+            for i=1:4
+                T(i,i) = 1;                
+            end
+            T(1,1) = cos(theta);
+            T(1,3) = sin(theta);
+            T(3,1) = -sin(theta);
+            T(3,3) = cos(theta);
         end
+        
+        function T = trotx(~, theta)
+            T = zeros(4,4);
+            for i=1:4
+                T(i,i) = 1;                
+            end
+            T(2,2) = cos(theta);
+            T(2,3) = sin(theta);
+            T(3,2) = -sin(theta);
+            T(3,3) = cos(theta);
+        end
+        
+        
+        
+       
     end
 end
 
